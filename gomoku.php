29bbaa9854db908b131fbeb7aad1d4968fde118e
mochix <at.mochix@gmail.com>
2015-09-08 22:56:44 +0900

    とりあえず交互に

--- gomoku.php
+++ gomoku.php
@@ -1,8 +1,15 @@
 <?php
 
-echo "It was placed in the center of the black!";
-echo "10の十";
+echo "It was placed in the center of the black!\n";
+echo "10の十\n\n";
+$i = 0;// Turn
 
-
-
-?>
+while (1) {
+	echo "Turn black : ";
+	$black[$i] = trim(fgets(STDIN)); // Enter wait
+// NG check
+	echo "Turn white : ";
+	$white[$i] = trim(fgets(STDIN)); // Enter wait
+// NG check
+$i++;
+}

e63ea33080a5f2a6c488b5dd338c4081c850d93c
mochix <at.mochix@gmail.com>
2015-09-19 22:43:03 +0900

    計算式を関数に

--- gomoku.php
+++ gomoku.php
@@ -1,42 +1,44 @@
 <?php
-// 19x19
 $point_array = array();
 echo "It was placed in the center of the black!\n";
 echo "10 to 10\n\n";
 $max_point = 19;
-$point_array = array_fill(0, $max_point * $max_point, "");
-$i = 0;// Turn;
+$point_array = array_fill(0, $max_point * $max_point, 0);
+$i = 0; // Turn;
 
-// var_dump($point_array);
+function point_calc($point_x, $max_point, $point_y)
+{
+    $point = $point_x * $max_point + $point_y;
+    return $point;
+}
 
 for ($j=0; $j <= count($point_array); $j++) {
     if ($i%2) {
         $turn = "white";
-        $stone = 1;
+        $stone = 2;
     } else {
         $turn = "black";
-        $stone = 0;
+        $stone = 1;
     }
+
     $range = array('min' => 0, 'max' => $max_point);
     echo $turn."X:";
-    $point_x = trim(fgets(STDIN));
+    $point_x = trim(fgets(STDIN)); // Enter
     if (filter_var($point_x, FILTER_VALIDATE_INT, array('options'=>$range)) === false) {
         echo "once again\n";
         continue;
     }
 
     echo $turn."Y:";
-    $point_y = trim(fgets(STDIN));
+    $point_y = trim(fgets(STDIN)); // Enter
     if (filter_var($point_y, FILTER_VALIDATE_INT, array('options'=>$range)) === false) {
         echo "once again\n";
         continue;
     }
 
-    $point = $point_x * $max_point + $point_y;
-
-    if ($point_array[$point] === '') {
+    if ($point_array[point_calc($point_x, $max_point, $point_y)] === '') {
         // echo "OK\n";
-        $point_array[$point] = $stone;
+        $point_array[point_calc($point_x, $max_point, $point_y)] = $stone;
     } else {
         echo "once again\n";
         continue;
@@ -44,5 +46,6 @@ for ($j=0; $j <= count($point_array); $j++) {
 
     $i++;
 }
+
 
 // print_r($point_array);

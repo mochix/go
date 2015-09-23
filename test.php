d2765248e0212fe10aa187ab61d18fd9842611e9
mochix <at.mochix@gmail.com>
2015-09-24 01:09:01 +0900

    テスト修正

--- test.php
+++ test.php
@@ -1,11 +1,17 @@
 <?php
 require_once 'utils.php';
-if (point_calc(0, 19, 0) != 0) {
+if (point_calc(0, 19, 0) == 0) {
+    // trigger_error('OK');
+} else {
     trigger_error('NG');
 }
-if (point_calc(18, 19, 18) != 360) {
+if (point_calc(18, 19, 18) == 360) {
+    // trigger_error('OK');
+} else {
     trigger_error('NG');
 }
-if (point_calc(14, 19, 8) != 274) {
+if (point_calc(14, 19, 8) == 274) {
+    // trigger_error('OK');
+} else {
     trigger_error('NG');
 }

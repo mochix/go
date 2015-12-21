d549a0a8c6f9bd403284c8250397ac6e7bb8327c
ripmochix <for.mochix@gmail.com>
2015-12-21 14:50:23 +0900

    ゲームクラスをForeachで回せるように
    作業中。入力位置が重複された場合、ちゃんと動かない。

--- gomoku.php
+++ gomoku.php
@@ -8,22 +8,19 @@ use Mochix\Go\Game;
 echo "It was placed in the center of the black!" . PHP_EOL;
 echo "10 to 10" . PHP_EOL;
 
-$board = new Board(19);
-$game  = new Game(19, Disk::DISK_BLACK);
-
-while(!$board->isFull()) {
-    $location = input($board, $game->getTurn());
+// ゲームループ.
+foreach((new Game(19, Disk::DISK_BLACK)) as $turn => $board) 
+{
+    $location = input($board, $turn);
     try {
-        if($board->setDisk($location['x'], $location['y'], $game->getTurn())){
+        if($board->setDisk($location['x'], $location['y'], $turn)) {
             break;
         }
     } catch (\InvalidArgumentException $e) {
         echo "NG" . PHP_EOL;
         continue;
     }
-    $game->next();
 }
-
 
 /** 入力関数 **/
 function input($board, $turn) {
--- lib/Game.php
+++ lib/Game.php
@@ -5,22 +5,35 @@ namespace Mochix\Go;
 use Mochix\Go\Disk;
 use Mochix\Go\Board;
 
-class Game {
-	
-	private $row = 0;
-
+/**
+ *
+ *
+ */
+class Game implements \IteratorAggregate {
 	private $turn = Disk::DISK_BLACK;
 
+
+	protected $board = null;
+
 	public function __construct($row = 19, $turn = Disk::DISK_BLACK) {
-		$this->row  = $row;
-		$this->turn = $turn;
+		$this->board = new Board($row);
+		$this->turn  = $turn;
 	}
+
+	public function getIterator()
+    {
+    	while(!$this->board->isFull())
+    	{
+    		yield $this->turn => $this->board;
+    		$this->next();
+    	}
+    }
 
 	public function getTurn() {
 		return $this->turn;
 	}
 
-	public function next() {
+	private function next() {
 		$this->turn = ($this->turn === Disk::DISK_BLACK) ? Disk::DISK_WHITE : Disk::DISK_BLACK;
 	}
 }
